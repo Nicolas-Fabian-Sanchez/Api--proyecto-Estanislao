46f8e256a314ea0677b13df20dcef3e9a1654bb3
Nicolas-Fabian-Sanchez <nicosanchez10@hotmail.com>
2023-06-19 00:34:03 -0300

    ultimo cambio

--- controllers/pedidoMenu.js
+++ controllers/pedidoMenu.js
@@ -3,8 +3,11 @@ const {cargaMesa,traerMenu}= require("../baseDatos/funcionesDB")
 //mandar el menu del resto ,guardar la mesa reservada y modificar base de datos mesa
 
 const pedidoMenu=async(req,res)=>{ // mando los datos de la mesa y envio el menu (para esto transformo los el array en dato json)
-       const {mesa,pedido,totalPagar} = req.body;
+       const mesa = req.body.mesa;
+       const pedido=[req.body.pedido];
+       const totalPagar=[req.body.totalPagar];
        let ingresarMesa = await cargaMesa({mesa,pedido,totalPagar});
+       console.log(pedido)
        console.log(ingresarMesa)//para ver la mesa recivida en consola ,no tiene funcion
        //let envioMenu = JSON.stringify(menu)//asi modifico el array en json para enviarlo
        res.redirect("/menuCompleto");// esto lo hago para nose preguntar jajaja?
